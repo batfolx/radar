aa8739692f789ed8a0f17901485085fdc46c5659
batfolx <victorvelea007@yahoo.com>
2021-01-26 20:50:38 -0500

    Adding radar

---
+++ radar.go
@@ -0,0 +1,36 @@
+package radar
+
+import (
+	"github.com/serial"
+)
+
+const MAX_BYTES = 1024
+
+func GetUSBDevice(usb string) (*serial.Port, error) {
+	config := &serial.Config{
+		Name:        usb,
+		Baud:        9600,
+	}
+
+	return serial.OpenPort(config)
+}
+
+
+func RecvDevice(usb *serial.Port, buffer []byte, until byte) (int, error) {
+	// keeps reading until last character is `until` character
+	offset := 0
+	for {
+		n, err := usb.Read(buffer[offset:])
+		offset += n
+		if offset > MAX_BYTES {
+			return 0, nil
+		}
+
+		if err != nil {
+			return 0, nil
+		} else if buffer[offset - 1] == until {
+			return offset, nil
+		}
+
+	}
+}
