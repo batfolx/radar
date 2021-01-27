f9490e2638f9343e7a6c49268768a6761deb764a
batfolx <victorvelea007@yahoo.com>
2021-01-26 21:06:19 -0500

    fixed dependencies

--- radar.go
+++ radar.go
@@ -1,20 +1,19 @@
 package radar
 
 import (
-	"github.com/serial"
+	"github.com/tarm/serial"
 )
 
 const MAX_BYTES = 1024
 
 func GetUSBDevice(usb string) (*serial.Port, error) {
 	config := &serial.Config{
-		Name:        usb,
-		Baud:        9600,
+		Name: usb,
+		Baud: 9600,
 	}
 
 	return serial.OpenPort(config)
 }
-
 
 func RecvDevice(usb *serial.Port, buffer []byte, until byte) (int, error) {
 	// keeps reading until last character is `until` character
@@ -28,7 +27,7 @@ func RecvDevice(usb *serial.Port, buffer []byte, until byte) (int, error) {
 
 		if err != nil {
 			return 0, nil
-		} else if buffer[offset - 1] == until {
+		} else if buffer[offset-1] == until {
 			return offset, nil
 		}
 
